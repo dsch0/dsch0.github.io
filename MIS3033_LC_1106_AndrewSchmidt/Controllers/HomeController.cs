b6b34ddb328ba54abb0d49bc4afa6fc71bd738e7
dsch0 <drew_schmidt@sbcglobal.net>
2023-11-06 10:15:23 -0600

    1106

---
+++ MIS3033_LC_1106_AndrewSchmidt/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIS3033_LC_1106_AndrewSchmidt.Controllers
+{
+    public class HomeController : Controller//name is home  
+    {
+        public int GetInt()
+        {
+            return 20;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
