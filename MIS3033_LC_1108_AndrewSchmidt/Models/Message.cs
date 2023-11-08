3b1b47aaccd34b25018f67ed2b7e4b2ff7fef067
dsch0 <drew_schmidt@sbcglobal.net>
2023-11-08 11:43:32 -0600

    Post lecture 1108

---
+++ MIS3033_LC_1108_AndrewSchmidt/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace MIS3033_LC_1108_AndrewSchmidt.Models
+{
+    public class Message
+    {
+        public string status { get; set; } = "success";
+
+        public string message { get; set; } = "";
+
+
+    }
+}
