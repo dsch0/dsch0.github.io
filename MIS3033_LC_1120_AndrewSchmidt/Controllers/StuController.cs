64410b6496622c44677ef1bd9c9c2949235a9d4e
dsch0 <drew_schmidt@sbcglobal.net>
2023-11-20 09:15:38 -0600

    1120

---
+++ MIS3033_LC_1120_AndrewSchmidt/Controllers/StuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIS3033002_LC_1115_AndrewSchmidt.Controllers
+{
+    public class StuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
