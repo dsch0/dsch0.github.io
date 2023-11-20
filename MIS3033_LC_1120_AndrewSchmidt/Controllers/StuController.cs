b896e3ff201c266900f09ac5d479b105f5b9a540
dsch0 <drew_schmidt@sbcglobal.net>
2023-11-20 11:40:55 -0600

    post1120

--- MIS3033_LC_1120_AndrewSchmidt/Controllers/StuController.cs
+++ MIS3033_LC_1120_AndrewSchmidt/Controllers/StuController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MIS3033002_LC_1115_AndrewSchmidt.Data;
 
 namespace MIS3033002_LC_1115_AndrewSchmidt.Controllers
 {
     public class StuController : Controller
     {
+        //IActionResult:View, web page
+        //JsonResult:Web api
+
+        StuDB db = new StuDB(); //stuDB, complex, member field
+
+        public JsonResult GetStus() //GetStus
+        {
+            return Json(db.Students);
+        }
+        
         public IActionResult Index()
         {
             return View();
