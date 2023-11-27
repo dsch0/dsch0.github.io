2cf7853749c1a91ee1f4a9715cefdae4d9f0f071
dsch0 <drew_schmidt@sbcglobal.net>
2023-11-27 09:34:35 -0600

    pre1127

---
+++ MIS3033_LC_1127_AndrewSchmidt/Controllers/StuController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MIS3033002_LC_1115_AndrewSchmidt.Data;
+
+namespace MIS3033002_LC_1115_AndrewSchmidt.Controllers
+{
+    public class StuController : Controller
+    {
+        //IActionResult:View, web page
+        //JsonResult:Web api
+
+        StuDB db = new StuDB(); //stuDB, complex, member field
+
+        public JsonResult GetStus() //GetStus
+        {
+            var r = db.Students.Select(x=>new {Id=x.Id,name=x.Name,favPlace=x.favPlace,DOB=x.DOB.ToString("MM/dd/yyyy"),lat=x.lat,lon=x.lon });
+            return Json(db.Students);
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
